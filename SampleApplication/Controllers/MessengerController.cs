886f40ea59ffd8c26a12a22bde29bc8d556767fc
Barker1889 <barker220489@gmail.com>
2016-03-30 19:21:30 +0100

    Converts messenger page to a razor page

---
+++ SampleApplication/Controllers/MessengerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using SampleApplication.Messaging;
+using SampleApplication.Models;
+
+namespace SampleApplication.Controllers
+{
+    public class MessengerController : Controller
+    {
+        private readonly SendMessageService _sendMessageService;
+
+        public MessengerController()
+        {
+            _sendMessageService = new SendMessageService();
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View("Index", new MessengerViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult SendMessage(MessengerViewModel model)
+        {
+            if (model.To.Length > 25 || !IsNumeric(model.To))
+            {
+                return RedirectToAction("Index", new { ErrorMessage = "Please enter a valid phone number" });
+            }
+
+            if (model.Body.Length > 612)
+            {
+                return RedirectToAction("Index", new { ErrorMessage = "Body length can only be 612 characters" });
+            }
+
+            _sendMessageService.SendMessage(User.Identity.Name, model.To, model.Body);
+
+            return RedirectToAction("Index", new {SuccessMessage = "Message sent successfully"});
+        }
+
+        private bool IsNumeric(string input)
+        {
+            return input.All(char.IsDigit);
+        }
+    }
+}
---
+++ SampleApplication/Models/MessengerViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SampleApplication.Models
+{
+    public class MessengerViewModel
+    {
+        [Required]
+        [MaxLength(20, ErrorMessage = "Max message length is 612 characters")]
+        public string To { get; set; }
+
+        [Required]
+        [MaxLength(612, ErrorMessage = "Max message length is 612 characters")]
+        public string Body { get; set; }
+    }
+}
