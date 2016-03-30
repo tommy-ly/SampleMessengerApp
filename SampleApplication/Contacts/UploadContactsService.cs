33262b28a70bdef877854d22cdfe2a2853938f1c
Barker1889 <barker220489@gmail.com>
2016-03-30 17:27:22 +0100

    UploadContacts: Adds functionality to upload contacts to the system

---
+++ SampleApplication/Contacts/UploadContactsService.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace SampleApplication.Contacts
+{
+    public class UploadContactsService
+    {
+        private readonly UserContactsService _contactsService;
+
+        public UploadContactsService()
+        {
+            _contactsService = new UserContactsService();
+        }
+
+        public bool ProcessFile(string fullFilePath, string username)
+        {
+            var contactsToCreate = new HashSet<UserContact>();
+
+            using (var file = File.Open(fullFilePath, FileMode.Open, FileAccess.Read))
+            using (var reader = new StreamReader(file, Encoding.UTF8))
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+
+                if (string.IsNullOrEmpty(line))
+                {
+                    continue;
+                }
+
+                var lineParts = line.Split(',');
+
+                var contactName = lineParts[0].Trim('"').Trim();
+                var phoneNumber = NumericOnly(lineParts[1]);
+
+                if (IsValidContact(contactName, phoneNumber))
+                {
+                    contactsToCreate.Add(new UserContact
+                    {
+                        ContactName = contactName,
+                        PhoneNumber = phoneNumber,
+                        Username = username
+                    });
+                }
+            }
+
+            if (!contactsToCreate.Any())
+            {
+                return false;
+            }
+
+            foreach (var userContact in contactsToCreate)
+            {
+                _contactsService.Create(userContact);
+            }
+
+            return true;
+        }
+
+        private bool IsValidContact(string contactName, string phoneNumber)
+        {
+            if (string.IsNullOrWhiteSpace(contactName) || contactName.Length > 20)
+            {
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(phoneNumber) || phoneNumber.Length > 25)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private static string NumericOnly(string input)
+        {
+            return Regex.Match(input, @"\d+").Value;
+        }
+    }
+}
---
+++ SampleApplication/Controllers/UploadContactsController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
+using SampleApplication.Contacts;
+
+namespace SampleApplication.Controllers
+{
+    public class UploadContactsController : Controller
+    {
+        private readonly string _uploadLocation;
+        private readonly UploadContactsService _uploadContactService;
+
+        public UploadContactsController()
+        {
+            _uploadLocation = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "UploadedFiles");
+
+            if (!Directory.Exists(_uploadLocation))
+            {
+                Directory.CreateDirectory(_uploadLocation);
+            }
+
+            _uploadContactService = new UploadContactsService();
+        }
+
+        [HttpGet]
+        public ActionResult Index(ErrorCode? errorCode)
+        {
+            if (errorCode.HasValue)
+            {
+                switch (errorCode.Value)
+                {
+                    case ErrorCode.UnsupportedFileType:
+                        ViewBag.ErrorMessage = "You can only upload text files";
+                        break;
+
+                    case ErrorCode.NoFileFound:
+                        ViewBag.ErrorMessage = "Please select a file to upload";
+                        break;
+
+                    case ErrorCode.Unknown:
+                        ViewBag.ErrorMessage = "Something went wrong, please try again";
+                        break;
+                }
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase file)
+        {
+            if (file == null)
+            {
+                return RedirectToAction("Index", new { errorCode = ErrorCode.NoFileFound });
+            }
+
+            if (file.ContentType != "text/plain")
+            {
+                return RedirectToAction("Index", new { errorCode = ErrorCode.UnsupportedFileType });
+            }
+
+            var fullFilePath = Path.Combine(_uploadLocation, file.FileName);
+            file.SaveAs(fullFilePath);
+
+            if (_uploadContactService.ProcessFile(fullFilePath, User.Identity.Name))
+            {
+                return RedirectToAction("List", "Contact", new { success = true });
+            }
+
+            return RedirectToAction("Index", new { errorCode = ErrorCode.Unknown });
+        }
+
+        public enum ErrorCode
+        {
+            UnsupportedFileType = 1,
+            Unknown = 2,
+            NoFileFound = 3
+        }
+    }
+}
